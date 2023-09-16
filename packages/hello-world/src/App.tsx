0c60d01c89e9656b977bd3809ac182e482f7ca38
Yoann Kermet <y.kermet@gmail.com>
2023-09-16 16:16:00 +0200

    package hello-world is now a react + typescript + vite project

---
+++ packages/hello-world/src/App.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+const App = (): React.ReactElement  => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Hello World!</h1>
+      <div>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
